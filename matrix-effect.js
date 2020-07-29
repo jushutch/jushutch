72838b0f866b319622253b5c9e31b8a9a801e884
jushutch <jushutch@umich.edu>
2020-07-29 10:15:14 -0400

    Working on alternative header

--- matrix-effect.js
+++ matrix-effect.js
@@ -4,21 +4,22 @@ const ctx = canvas.getContext('2d');
 const w = canvas.width = document.body.offsetWidth;
 const h = canvas.height = document.body.offsetHeight;
 
-ctx.fillStyle = '#000';
+//ctx.fillStyle = '#000';
+ctx.fillStyle = '#ffffff';
 ctx.fillRect(0, 0, w, h);
 
-const cols = Math.floor(w / 20) + 1;
+const cols = Math.floor(w / 50) + 1;
 const ypos = Array(cols).fill(0);
 
 function matrix() {
-    ctx.fillStyle = "rgba(0, 0, 0, .2)";
+    ctx.fillStyle = "rgba(255, 255, 255, .2)";
     ctx.fillRect(0, 0, w, h);
     ctx.fillStyle = '#0f0';
     ctx.font = '15pt monospace';
 
     ypos.forEach((y, ind) => {
         const text = String.fromCharCode(Math.random() * 128);
-        const x = ind * 20;
+        const x = ind * 50;
         ctx.fillText(text, x, y);
         if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
         else ypos[ind] = y + 20;
