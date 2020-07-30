b1804acee7e8e925f2531eaf70192f233a1a83c4
jushutch <jushutch@umich.edu>
2020-07-29 22:21:53 -0400

    Adding python script to convert mp4 to gif

--- mp4togif.py
+++ mp4togif.py
@@ -7,7 +7,7 @@ inputFilename = str(sys.argv[1]);
 os.system("cp " + inputFilename + " temp.mp4")
 ff = FFmpeg(
         inputs = {'temp.mp4' : None},
-        outputs = {'output.gif': '-vf "fps=30,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse"'}
+        outputs = {'output.gif': '-vf "fps=10,scale=1000:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse"'}
         )
 print(ff.cmd)
 ff.run()
